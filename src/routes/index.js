1fda0e15aa2cd2c3129141c36bac73773da85e2c
Marcos Alves <marcoveizalvess@gmail.com>
2023-11-03 23:11:35 -0300

    Modules, architecture and pages basics

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, ActivityIndicator } from 'react-native'
+import AuthRoutes from './auth.routes'
+import AppRoutes from './app.routes'
+
+export default function Routes() {
+    const signed = true
+    const loading = false
+    if (loading) {
+        <View style={{ flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#36393F'}}>
+            <ActivityIndicator size={50} color="#E52246"/>
+        </View>
+    }
+    return(
+        signed ? <AppRoutes/> : <AuthRoutes/>
+    )
+}
